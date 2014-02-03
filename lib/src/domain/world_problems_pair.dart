5b6ea20cce5fd2d181aa18663c6055d863ae23e4
Jirka Penzes <jirkapenzes@gmail.com>
2014-02-03 22:01:38 +0100

    Added domain objects

---
+++ lib/src/domain/world_problems_pair.dart
@@ -0,0 +1,10 @@
+part of world_problems;
+
+class WorldProblemsPair {
+  
+  final WorldProblem firtProblem; 
+  final WorldProblem thirdProblem; 
+
+  WorldProblemsPair(this.firstProblem, this.thirdProblem) { }
+}
+
