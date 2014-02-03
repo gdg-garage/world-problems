af0f8d7d5fdd46bb2a272ad300837501c246147f
Jirka Penzes <jirkapenzes@gmail.com>
2014-02-03 22:17:29 +0100

    Added test for fetcher

--- lib/src/domain/world_problems_pair.dart
+++ lib/src/domain/world_problems_pair.dart
@@ -2,7 +2,7 @@ part of world_problems;
 
 class WorldProblemsPair {
   
-  final WorldProblem firtProblem; 
+  final WorldProblem firstProblem; 
   final WorldProblem thirdProblem; 
 
   WorldProblemsPair(this.firstProblem, this.thirdProblem) { }
