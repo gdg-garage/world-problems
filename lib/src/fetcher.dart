36adafa3af7c562601da2bba81520078458e468e
Filip Hracek <filiph@google.com>
2014-02-17 19:13:21 +0100

    Link srcs

--- lib/src/fetcher.dart
+++ lib/src/fetcher.dart
@@ -1,6 +1,7 @@
 part of world_problems;
 
 class Fetcher {
+  /// Top-level URL of the API.
   final String url;
   
   Fetcher(this.url);
--- lib/world_problems.dart
+++ lib/world_problems.dart
@@ -1,4 +1,6 @@
 library world_problems;
 
 part "src/world_problems_app.dart";
-
+part "src/domain/world_problem.dart";
+part "src/domain/world_problems_pair.dart";
+part "src/fetcher.dart";
