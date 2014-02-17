435975464d94f903bcf08747b35c79d213b490bd
Filip Hracek <filiph@google.com>
2014-02-17 20:36:25 +0100

    Implement ProblemView

--- lib/src/view/page_view.dart
+++ lib/src/view/page_view.dart
@@ -2,9 +2,15 @@ part of world_problems;
 
 class PageView {
   final Element reload;
-  final Element first;
-  final Element third;
-  PageView(this.reload, this.first, this.third);
+  final ProblemView first;
+  final ProblemView third;
+  PageView(this.reload, Element firstElement, Element thirdElement) :
+    first = new ProblemView(firstElement),
+    third = new ProblemView(thirdElement);
   
   
+  void update(WorldProblemsPair pair) {
+    first.update(pair.firstProblem);
+    third.update(pair.thirdProblem);
+  }
 }
--- lib/src/view/problem_view.dart
+++ lib/src/view/problem_view.dart
@@ -1,5 +1,21 @@
 part of world_problems;
 
 class ProblemView {
+  Element text;
+  Element author;
+  AnchorElement sourceAnchor;
   
+  
+  ProblemView(Element el) {
+    text = el.querySelector(".text");
+    author = el.querySelector(".author");
+    sourceAnchor = el.querySelector(".url");
+  }
+  
+  void update(WorldProblem problem) {
+    text.text = problem.text;
+    author.text = "– ${problem.author}";  // TODO: move mdash to HTML
+    sourceAnchor.href = problem.url;
+    sourceAnchor.text = problem.source;
+  }
 }
