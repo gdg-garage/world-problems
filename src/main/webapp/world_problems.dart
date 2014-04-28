b7ef1a6589054b8c9149bd7018c99527933ff516
Filip Hracek <filiph@google.com>
2014-04-28 21:14:14 +0200

    Force add generated files

---
+++ src/main/webapp/world_problems.dart
@@ -0,0 +1,13 @@
+import "package:world_problems/world_problems.dart";
+
+main() {
+  String DEBUG_URL = "../test/data/sample_data.json";
+  String fetchUrl = "/_ah/api/problems/v1/random";
+  Fetcher fetcher = new Fetcher(fetchUrl);
+  
+  WorldProblemsApp app = new WorldProblemsApp(fetcher);  
+  app.initialize('#reload-button', '.first-world', '.third-world'); 
+  app.start();
+}
+
+
