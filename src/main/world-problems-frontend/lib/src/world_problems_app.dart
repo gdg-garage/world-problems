6c75615e6d9e66030d06c76c5a237c4a2e19a2d1
Filip Hracek <filiph@google.com>
2014-06-09 20:51:42 +0200

    Remove pulse class only after it had time to run the animation. Stop button from jumping on the page (hack)

--- src/main/world-problems-frontend/lib/src/world_problems_app.dart
+++ src/main/world-problems-frontend/lib/src/world_problems_app.dart
@@ -1,66 +1,70 @@
 part of world_problems;
 
 class WorldProblemsApp {
-   
+
   Fetcher fetcher;
   PageView page;
   bool isInitialized;
   Queue<WorldProblem> firstProblemsHistory;
   Queue<WorldProblem> thirdProblemsHistory;
   final int maxHistory;
-  
+
   DivElement _reloadDiv;
   AnchorElement reload;
-  
-  WorldProblemsApp(this.fetcher, {this.maxHistory: 1}){   
+
+  static const pulseAnimationDuration = const Duration(seconds: 1, milliseconds:
+      200);
+
+  WorldProblemsApp(this.fetcher, {this.maxHistory: 1}) {
     isInitialized = false;
     firstProblemsHistory = new Queue();
     thirdProblemsHistory = new Queue();
   }
-  
-	void initialize(String buttonId, String firstId, String thirdId) { 
-	  reload = querySelector(buttonId);
-	  _reloadDiv = querySelector(".button");  // TODO: setting
-	  Element first = querySelector(firstId);
-	  Element second = querySelector(thirdId);
-	  page = new PageView(reload, first, second);
-	  reload.onClick.listen((MouseEvent ev) {
-	    _reloadDiv.classes.add("pulse");
-  	  refreshPair();
-  	  ev.preventDefault();  // Prevent from reloading the page.
-	  });   
-	  isInitialized = true;
+
+  void initialize(String buttonId, String firstId, String thirdId) {
+    reload = querySelector(buttonId);
+    _reloadDiv = querySelector(".button"); // TODO: setting
+    Element first = querySelector(firstId);
+    Element second = querySelector(thirdId);
+    page = new PageView(reload, first, second);
+    reload.onClick.listen((MouseEvent ev) {
+      _reloadDiv.classes.add("pulse");
+      new Timer(pulseAnimationDuration, () {
+        _reloadDiv.classes.remove("pulse");
+      });
+      refreshPair();
+      ev.preventDefault(); // Prevent from reloading the page.
+    });
+    isInitialized = true;
   }
-	
-  void start() { 
+
+  void start() {
     if (!isInitialized) {
       throw new Exception("World problems not initialized");
     }
   }
-  
+
   void refreshPair() {
-    getRandomPair()
-      .then((WorldProblemsPair pair) {
-        updatePage(pair);
-        _reloadDiv.classes.remove("pulse");
-      });
-  }
-  
-  Future<WorldProblemsPair> getRandomPair() {
-    return fetcher.fetchRandom(firstProblemsHistory, thirdProblemsHistory)
-      .then((WorldProblemsPair pair) {
-        firstProblemsHistory.addLast(pair.firstProblem);
-        if (firstProblemsHistory.length > maxHistory) {
-          firstProblemsHistory.removeFirst();
-        }
-        thirdProblemsHistory.addLast(pair.thirdProblem);
-        if (thirdProblemsHistory.length > maxHistory) {
-          thirdProblemsHistory.removeFirst();
-        }
-        return pair;
+    getRandomPair().then((WorldProblemsPair pair) {
+      updatePage(pair);
     });
   }
-  
+
+  Future<WorldProblemsPair> getRandomPair() {
+    return fetcher.fetchRandom(firstProblemsHistory, thirdProblemsHistory).then(
+        (WorldProblemsPair pair) {
+      firstProblemsHistory.addLast(pair.firstProblem);
+      if (firstProblemsHistory.length > maxHistory) {
+        firstProblemsHistory.removeFirst();
+      }
+      thirdProblemsHistory.addLast(pair.thirdProblem);
+      if (thirdProblemsHistory.length > maxHistory) {
+        thirdProblemsHistory.removeFirst();
+      }
+      return pair;
+    });
+  }
+
   void updatePage(WorldProblemsPair pair) {
     page.update(pair);
   }
