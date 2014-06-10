9b633986d46f7d96aa7afe34450deff5ab18fc06
Filip Hracek <filiph@google.com>
2014-06-10 16:18:11 +0200

    Move Dart build to maven

--- src/main/world-problems-frontend/build.sh
+++ src/main/world-problems-frontend/build.sh
@@ -1,7 +1,14 @@
+# Go to this script's directory.
+cd "$(dirname "$0")"
+# Change according to where you have your dart pub file.
 /Applications/dart/dart-sdk/bin/pub build web --mode=debug
+# Copy files to webapp/.
 cp -R build/web/* ../webapp/
+# Remove the build directory.
 rm -fr build/
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home
-cd ../../../
-mvn package
-cd -
+
+# LEGACY: run maven (now, it's the other way around - maven runs this script)
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home
+#cd ../../../
+#mvn package
+#cd -
